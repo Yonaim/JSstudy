a44ab67520c12ffcaa015e1020b6fd87e4b61a71
yonazang <yonaimyo@gmail.com>
2022-09-07 02:17:20 +0900

    promise 추가

--- promise/promise2.js
+++ promise/promise2.js
@@ -55,18 +55,37 @@ fetchNumber.then(result => result * 2)
 // .then 메서드에 넘겨주는 함수가 새로운 Promise 객체를 반환하면 해당 Promise 객체가 .then 메서드의 결과로 반환된다!
 // 즉 위의 예시에서 최종적으로 출력되는 값은 (5 * 2 * 3) - 9 = 21이다.
 
-// 4. Error Handling
+
+// ---------- 4. Error Handling ----------
 const getHen = () =>
 	new Promise((resolve, reject) => {
-		setTimeout(() => resolve('get Hen!'), 1000);
+		console.log('test');
+	setTimeout(() => resolve('HEN'), 1000);
 	});
 const getEgg = (Hen) =>
 	new Promise((resolve, reject) => {
-		setTimeout(() => resolve(`Using ${Hen}, We get Egg!`), 1000);
+		setTimeout(() => 
+		// resolve(`${Hen} => EGG`)
+		reject(new Error('OOPS, getEgg Error!'))
+		, 1000);
 	});
 const cook = (egg) =>
 	new Promise((resolve, reject) => {
 		setTimeout(() => {
-			resolve(`Wow ${egg} is now delicious Meal!`);
+			resolve(`Wow ${egg} is now delicious Meal !`);
+			// reject(new Error('OOPS, cook Error!'))
 		}, 1000);
 	});
+// 이렇게 Promise 객체가 함수 안에 지역 변수로 감싸져(?) 있으면 (당연히) 바로 실행되지 않음. 함수가 호출되어야 프로미스의 executor도 실행됨
+
+getHen()
+	.then(getEgg)
+	.catch((error) => {
+		return ('BREAD');
+	})
+	.then(cook)
+	.then(console.log)
+	.catch(console.log);
+// 하나의 리턴만 받아서 그대로 전달하는 경우에는 익명 함수를 작성할 필요 없이 그냥 바로 함수를 전달해도 됨
+// 앞쪽에서 일어난 error도 맨 마지막에서 catch할 수 있음!!!!! 
+	// (반환되는 것이 error라면 then 메서드는 실행되지 않고 catch가 받을때까지 뒤로 넘겨짐)
